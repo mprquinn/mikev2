b3ba01b597dff9a17f5b4e000914db22daaf0f2c
Mike <mprquinn@gmail.com>
2013-07-07 20:52:40 -0600

    Fix for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,12 @@ $(document).ready(function(){
 		'height' : windowHeight + 'px'
 	});
 	
-	// $(window).resize(function(){
-	// 	windowWidth = $(window).innerWidth();
-	// 	$('.callout h2').text(windowWidth + 'px');
-	// });
 
 
 	$('.home .scroll').click(function(){
 		$(window).scrollTo('.work', 1000);
 	});
 
-	// var scrollDistance = 0;
-
-	// $(window).scroll(function(){
-	// 	scrollDistance = $(window).scrollTop();
-	// 	console.log(scrollDistance);
-	// });
 
 	
 });
