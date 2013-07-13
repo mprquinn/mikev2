f25f4e1694e47bff21ef26185d4a9cc2ac329d2a
Mike Quinn <mprquinn@gmail.com>
2013-07-13 13:25:06 -0600

    More fixes

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,26 @@ $(document).ready(function(){
 		$(window).scrollTo('.work', 1000);
 	});
 
+	function getBlog(){
+		var blog = tumblr_api_read;
+		var length = 500;
+		var posts = 1;
+		var date = '';
+		var post = '';
+		var link = '';
 
-	
+		for(i=0; i<posts; i++){
+			date = blog.posts[i].date;
+			post = blog.posts[i]['regular-body'];
+			link = blog.posts[i].url;
+			$('.blog .date').html(date);
+			$('.blog .post').html(post);
+			$('.blog .post').append('<p><a class="button" href="' + link +'" target="_blank">' + 'View on Tumblr' +'</a></p>');
+		}
+
+		console.log(date + ' ' + post + ' ' + link);
+	}
+
+	getBlog();
+	console.log('test');
 });
